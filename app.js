65be424816e48e71d947ab82a4fc6554cebde799
Una Kravets <unakravets@unakravets-imacpro.roam.corp.google.com>
2019-05-15 16:00:33 -0400

    initial commit setup

---
+++ app.js
@@ -0,0 +1,32 @@
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDrawer} from "@material/drawer";
+import {MDCList} from "@material/list";
+
+// Select DOM elements
+
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const drawerElement = document.querySelector('.mdc-drawer');
+const mainContentEl = document.querySelector('.main-content');
+
+// Initialize top app bar
+
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+// Initialize drawer
+
+const initModalDrawer = () => {
+  drawerElement.classList.add("mdc-drawer--modal");
+  const drawer = MDCDrawer.attachTo(drawerElement);
+  drawer.open = false;
+  topAppBar.setScrollTarget(mainContentEl);
+  topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+  });
+
+  listEl.addEventListener('click', (event) => {
+    drawer.open = false;
+  });
+}
+
+initModalDrawer();
